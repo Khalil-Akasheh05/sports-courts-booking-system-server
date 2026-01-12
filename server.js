ba16f68d6e1a09a55000b1e57f455b472e2c9fa1
Khalil-Akasheh05 <khalilakasheh2005@gmail.com>
2026-01-12 21:46:22 +0300

    Connect routes to server

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import pgClient from "./db.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import authenticationRoutes from "./routes/authenticationRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +15,10 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/admin", adminRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authenticationRoutes)
 
 pgClient.connect().then(() => {
   app.listen(PORT, () => {
